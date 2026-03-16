059a888e0825f1c1e3ba28388bd6224bb5c620b9
Chits <77530892+callmechits@users.noreply.github.com>
2026-03-16 10:27:23 +0530

    Set GS_WEBHOOK_URL to the deployed Apps Script URL

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // Public client config.
 // Paste your deployed Apps Script Web App URL below.
-window.GS_WEBHOOK_URL = window.GS_WEBHOOK_URL || '';
+window.GS_WEBHOOK_URL = 'https://script.google.com/macros/s/AKfycbw3G_LyOnYiDXhNM2oGVJK1fYNky2GhtDOFWscFk5vVIi0SaRAMKNq8Ytat9KkRJic/exec';
