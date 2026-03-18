34b9c3a67e8ac3a100127f3c2b48d8af3c8cc4d5
Chits <77530892+callmechits@users.noreply.github.com>
2026-03-18 17:52:16 +0530

    Delete js/config.js

--- js/config.js
+++
@@ -1,3 +0,0 @@
-// Public client config.
-// Paste your deployed Apps Script Web App URL below.
-window.GS_WEBHOOK_URL = 'https://script.google.com/macros/s/AKfycbw9LJGiUoZh26KKYB1Ll-k08NfpLFCgOfIBA64NoODGrkY9IM1wNm-dR3WtT7MqVfIu/exec';
